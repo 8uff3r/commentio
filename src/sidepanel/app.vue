c6f7be63426016d7d2ab85ff58f8c07d4c6a4e82
8uff3r <8uff3r@gmail.com>
2024-08-21 16:34:20 +0330

    fixes

--- src/sidepanel/app.vue
+++ src/sidepanel/app.vue
@@ -4,7 +4,7 @@ const store = useAuthStore()
 <template>
   <template v-if="store.isAuthed">
     <img
-      src="/logo.webp"
+      src="/logo.png"
       class="object-cover h-10 p-1 mx-auto select-none"
     />
     <RouterView />
@@ -14,7 +14,7 @@ const store = useAuthStore()
       class="p-2 flex flex-col justify-center h-[100svh] w-full items-center gap-3"
     >
       <img
-        src="/logo.webp"
+        src="/logo.png"
         class="object-cover w-full max-w-52 p-1 mx-auto select-none"
       />
       <Auth />
